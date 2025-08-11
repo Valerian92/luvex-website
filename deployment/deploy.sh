c46942498cf52e2a6ddd970fe11795aedda027f4
Valerian92 <valerian.luvex@gmail.com>
2025-08-11 20:17:31 +0200

    Delete deploy.sh

--- deployment/deploy.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-echo "🌐 Deploying LUVEX Website..."
-cd /opt/bitnami/wordpress/wp-content/themes/luvex-theme
-sudo chown -R valerian:valerian .
-git pull origin develop  
-sudo chown -R bitnami:daemon .
-echo "✅ Website deployed successfully!"
